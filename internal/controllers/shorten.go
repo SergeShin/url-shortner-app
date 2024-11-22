2f41cf78456ddfa807a622fa47545faa57868c0c
Serhii Shynkarenko <sshinkarenko@gmail.com>
2024-11-22 23:23:55 +0100

    added templates

---
+++ internal/controllers/shorten.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"html/template"
+	"net/http"
+	"strings"
+)
+
+func Shorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	originalUrl := r.FormValue("url")
+	if originalUrl == "" {
+		http.Error(w, "URL not provided", http.StatusBadRequest)
+		return
+	}
+
+	if !strings.HasPrefix(originalUrl, "http://") && !strings.HasPrefix(originalUrl, "https://") {
+		originalUrl = "https://" + originalUrl
+	}
+
+	data := map[string]string{
+		"ShortURL": originalUrl,
+	}
+
+	t, err := template.ParseFiles("internal/views/shorten.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err = t.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
